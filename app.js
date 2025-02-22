00236cf18a6a5fde4cd134591b1ba99f4b9f6167
Prateek <prateek.g@navgurukul.org>
2025-02-22 11:40:34 +0530

    Connect to a MongoDB database with Mongoose

--- app.js
+++ app.js
@@ -5,6 +5,9 @@ import logger from "morgan";
 import indexRouter from "./routes/index.js";
 import usersRouter from "./routes/users.js";
 
+import mongoose from 'mongoose';
+mongoose.connect(process.env.DATABASE_URL || 'mongodb://localhost/your-app-name');
+
 const app = express();
 
 app.use(logger("dev"));
